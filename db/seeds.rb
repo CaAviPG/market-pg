4f7c5143c46836e02099f00cba2d109fbf423387
Caren Pampas <carenpg15@gmail.com>
2022-07-11 15:55:13 -0500

    change images

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,90 +18,44 @@ usuario2 = User.create(email: "alison@gmail.com", password: "Alison123", passwor
 
 
 a = Product.create(
-       name: "Tequeños de chocolate",
-       description: "Los tequeños de chocolate son una masa frita rellena de chocolate fundido, precio por docena.",
+       name: "Keyboard",
+       description: "Teclado inhalambrico",
        price: 5000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a2V5Ym9hcmR8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
 b = Product.create(
-       name: "Pastel tres leches",
-       description: "El pastel tres leches es un bizcocho jugoso bañado en combinación de 3 leches cubierta de merengue, precio por unidad.",
+       name: "Mouse",
+       description: "Mouse inhalambrico",
        price: 7000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1648726131376-bf948d201550?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdXNlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
 
 )
 
 c = Product.create(
-       name: "Pasta seca",
-       description: "Las pastas secas son una especialidad de repostería, son pequeñas galletas cocidas al horno, hecho con una pasta a base de harina, mantequilla, huevos, y, azúcar o sal, precio por docena.",
+       name: "Headphones",
+       description: "Llévalo contigo a donde quieras",
        price: 4000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1638803782506-d975a6809f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhlYWRwaG9uZXN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
 d = Product.create(
-       name: "Palmerita",
-       description: "Las palmeritas son unas galletas hechas a base de masa de hojaldre preparadas con una curiosa forma de 'oreja' cubiertas por azucar, precio por docena.",
+       name: "Watch",
+       description: "Llega a la hora correcta y haz realidad tus metas",
        price:4000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1587914839172-657ff0b85b16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2F0Y2h8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
 e = Product.create(
-       name: "Mandoca",
-       description: "Masa de maiz dulce frita, precio por docena.",
+       name: "Camera",
+       description: "Retrata tus mejores momentos",
        price: 3000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1522124831747-501d9202d3c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGNhbWVyYXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
 )
 
 f = Product.create(
-       name: "Pastel de calabaza",
-       description: "Pastel hecho de una mezcla de bizcocho tradicional y calabaza",
+       name: "Dron",
+       description: "Captura mejores imágenes con la ayuda de este aparato.",
        price: 7000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-g = Product.create(
-       name: "Golfeado",
-       description: "El golfeado es un pan dulce típico de la gastronomía de Venezuela, originario de la Región Capital del país, especialmente de la zona de los Altos Mirandinos, en particular de Carrizal, precio por unidad.",
-       price: 3000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-h = Product.create(
-       name: "Pan de guayaba",
-       description: "El pan de guayaba es un pan dulce horneado relleno de una jalea de fruta(guayaba) endulzada, precio por unidad.",
-       price: 2000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-
-i = Product.create(
-       name: "Brownie",
-       description: "El brownie es un bizcocho de mezcla de chocolate con trozos de chocolate, precio por unidad",
-       price: 1500,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-
-j = Product.create(
-       name: "Chip Cookies",
-       description:"Galleta con receta tradicional de una masa compuesta de mantequilla con azúcar marena y azucar blanca con trozos de chocolate semi-dulces, precio por docena",
-       price: 5000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-
-k = Product.create(
-       name: "Cheesecake",
-       description: "Tarta con fondo de galleta crujiente rellena de queso crema",
-       price: 7000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-)
-
-
-l = Product.create(
-       name: "Pie de limon",
-       description: "El pie de limon es una tarta formada por una base de masa de hojaldre que se rellena con crema de limón y es cubierta por merengue",
-       price: 7000,
-       imagen:"https://images.unsplash.com/photo-1593078166039-c9878df5c520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1541504619657-2a60d7301207?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fGNhbWVyYXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
 )
