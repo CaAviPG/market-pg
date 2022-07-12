3ad0915759b978bcf71853e3c9ba07b686b8701a
Caren Pampas <carenpg15@gmail.com>
2022-07-11 19:08:21 -0500

    cambio de imagen y banner1

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ usuario2 = User.create(email: "alison@gmail.com", password: "Alison123", passwor
 # usuario4 = User.create(email: "midory@gmail.com", password: "Midory123", password_confirmation: "Midory123", name: "Midory")
 
 
-a = Product.create(
+Product.create(
        name: "Keyboard",
        description: "Teclado inhalambrico",
        price: 5000,
        imagen:"https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a2V5Ym9hcmR8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
-b = Product.create(
+Product.create(
        name: "Mouse",
        description: "Mouse inhalambrico",
        price: 7000,
@@ -32,30 +32,30 @@ b = Product.create(
 
 )
 
-c = Product.create(
+Product.create(
        name: "Headphones",
        description: "Llévalo contigo a donde quieras",
        price: 4000,
        imagen:"https://images.unsplash.com/photo-1638803782506-d975a6809f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhlYWRwaG9uZXN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
-d = Product.create(
+Product.create(
        name: "Watch",
        description: "Llega a la hora correcta y haz realidad tus metas",
        price:4000,
        imagen:"https://images.unsplash.com/photo-1587914839172-657ff0b85b16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2F0Y2h8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 )
 
-e = Product.create(
+Product.create(
        name: "Camera",
        description: "Retrata tus mejores momentos",
        price: 3000,
        imagen:"https://images.unsplash.com/photo-1522124831747-501d9202d3c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGNhbWVyYXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
 )
 
-f = Product.create(
-       name: "Dron",
-       description: "Captura mejores imágenes con la ayuda de este aparato.",
+Product.create(
+       name: "Tablet",
+       description: "Conectate con una mejor navegación",
        price: 7000,
-       imagen:"https://images.unsplash.com/photo-1541504619657-2a60d7301207?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fGNhbWVyYXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
+       imagen:"https://images.unsplash.com/photo-1644953798828-a92178929505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dGFibGV0fGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
 )
